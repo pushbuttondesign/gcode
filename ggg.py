89410fcf7f461c1783218438f30cf6bc0cbdea5c
pushbutton <steve@pushbutton.com>
2023-01-24 23:03:31 -0800

    updated x and y only modes

--- ggg.py
+++ ggg.py
@@ -15,84 +15,139 @@ accepts only X and only Y for half grids
 assumes XY start position will be set manually
 
 USAGE
-ggg.py -x 100 -y 100 -p 10 -f 3000 > out.gcode
+ggg.py -x -100 -y 100 --x-pitch -10 --y-pitch 10 -f 500 -z -10 --only-y > out.gcode
 """
 
 import argparse
 parser = argparse.ArgumentParser(description='generate gcode for drawing grids')
 parser.add_argument('--x-max', '-x', type=int, required=True, help='X working width in mm')
 parser.add_argument('--y-max', '-y', type=int, required=True, help='Y working width in mm')
-parser.add_argument('--pitch', '-p', type=int, required=True, help='Pitch between grid (stepover distance) in mm')
-parser.add_argument('--feed', '-f', type=int, required=True, help='Horizontal feed in mm per minute')
-parser.add_argument('--z-depth', '-z', type=float, default=0, help='Z depth to cut (0)')
-parser.add_argument('--safe', '-s', type=float, default=5, help='Safe Z height (5)')
-parser.add_argument('--only-x', default=False, action='store_true', help='Surface only in X (Default both)')
-parser.add_argument('--only-y', default=False, action='store_true', help='Surface only in Y (Default both)')
+parser.add_argument('--x-pitch', '-a', type=int, required=True, help='pitch between grid (stepover distance) in mm')
+parser.add_argument('--y-pitch', '-b', type=int, required=True, help='pitch between grid (stepover distance) in mm')
+parser.add_argument('--feed', '-f', type=int, required=True, help='horizontal feed in mm per minute')
+parser.add_argument('--z-depth', '-z', type=float, default=0, help='Z depth to cut (default 0 for air cut)')
+parser.add_argument('--only-x', default=False, action='store_true', help='surface only in X (default both)')
+parser.add_argument('--only-y', default=False, action='store_true', help='surface only in Y (default both)')
 args = parser.parse_args()
 v = vars(args)
 
+#argument validation
+if args.x_pitch * args.x_max * args.y_max * args.feed == 0:
+    raise ValueError('Input values must be non-zero numbers0')
+if args.x_max < 0 and args.x_pitch >= 0 or args.x_max >=0 and args.x_pitch < 0:
+    raise ValueError('Both axis max & pitch must have the same sign')
+if args.y_max < 0 and args.y_pitch >= 0 or args.y_max >=0 and args.y_pitch < 0:
+    raise ValueError('Both axis max & pitch must have the same sign')
+if args.feed < 500 or args.feed > 1500:
+    print("Are you sure a feed rate of {} mm per minute is correct?".format(args.feed))
+    input('press any key to continue')
+if args.z_depth > 80:
+    print("Are you sure a Z depth of {} mm is correct?".format(args.z_depth))
+    input('press any key to continue')
+
 #g-code header
+print('; HEADER')
+print('; AUTHOR: export from ggg.py')
+print('; PART #: grid-01-01')
 print('G54; Work Coordinates')
 print('G21; mm mode')
 print('G90; absolute positioning')
 print('G28 z; home z axis')
-print(';G28 x y; home x y at the same time - not used, assume set start positoin manually ')
-print('G0 Z{}; rapid move to specified safe z height'.format(args.safe))
+print(';G28 x y; home x y at the same time - not used, assume set start position manually ')
 print('M3 S3000 $-1; start all spindles clockwise')
 print('F{}; set feedrate'.format(args.feed))
 
-#list x, y locations of  each row/column
-pitch = args.pitch
-def steps(end):
+#return list of cordinates between 0 and end by pitch
+def steps(end, pitch):
     s = [x for x in range(0, end, pitch)]
     #if end not a multiple of pitch then append end position
     if s[-1] != end:
         s.append(end)
     return s
 
-x_steps = steps(args.x_max)
-y_steps = steps(args.y_max)
+x_steps = steps(args.x_max, args.x_pitch)
+y_steps = steps(args.y_max, args.y_pitch)
 
-#if odd number of steps, duplicate last value
+#if odd number of steps, append None
 if len(x_steps) % 2 != 0:
-    x_steps.append(x_steps[-1])
+    x_steps.append(None)
 if len(y_steps) % 2 != 0:
-    y_steps.append(y_steps[-1])
+    y_steps.append(None)
 
 #split into pairs
 def pairs(steps):
     a = iter(steps)
     return zip(a, a)
 
-x_steps = pairs(x_steps)
-y_steps = pairs(y_steps)
+x_pairs = pairs(x_steps)
+x_reverse = pairs(reversed(x_steps))
+y_pairs = pairs(y_steps)
 
-#if y only mode false, cut x axis
-if not args.only_y:
+#keep track of positions
+x_cur = 0
+y_cur = 0
+
+print('G1 Z{} F200; slowly drop cutter to final cut depth'.format(args.z_depth))
+
+if args.only_y == False:
     print('')
     print('; X AXIS')
-    print('G1 Z{} F500; slowly drop cutter into work piece'.format(args.z_depth))
-    for a, b in y_steps:
-        print('G1 Y{}'.format(a))
-        print('G1 X{}'.format(args.x_max))
-        print('G1 Y{}'.format(b))
-        print('G1 X0')
-    print('G0 Z{}; rapid lift cutter'.format(args.safe))
-    print('G0 Y0; rapid reset y')
+    for a, b in y_pairs:
+        if b != None:
+            print('G1 Y{}'.format(a))
+            print('G1 X{}'.format(args.x_max))
+            print('G1 Y{}'.format(b))
+            print('G1 X0')
+            #save ending positions
+            y_cur = b
+            x_cur = 0
+        else:
+            print('G1 Y{}'.format(a))
+            print('G1 X{}'.format(args.x_max))
+            #save ending positions
+            y_cur = a
+            x_cur = args.x_max
 
-#if x only mode false, cut y axis
-if not args.only_x:
+if args.only_x == False:
     print('')
     print('; Y AXIS')
-    print('G1 Z{} F500; slowly drop cutter into work piece'.format(args.z_depth))
-    for a, b in x_steps:
-        print('G1 X{}'.format(a))
-        print('G1 Y{}'.format(args.y_max))
-        print('G1 X{}'.format(b))
-        print('G1 Y0')
+    #if starting at X0 Y0 as y only mode is active
+    if x_cur == 0 and y_cur == 0:
+        for a, b in x_pairs:
+            if b != None:
+                print('G1 X{}'.format(a))
+                print('G1 Y{}'.format(args.y_max))
+                print('G1 X{}'.format(b))
+                print('G1 Y0')
+            else:
+                print('G1 X{}'.format(a))
+                print('G1 Y{}'.format(args.y_max))
+    #if starting at X0 YMAX
+    elif x_cur == 0:
+        for a, b in x_pairs:
+            if b != None:
+                print('G1 X{}'.format(a))
+                print('G1 Y0')
+                print('G1 X{}'.format(b))
+                print('G1 Y{}'.format(args.y_max))
+            else:
+                print('G1 X{}'.format(a))
+                print('G1 Y0')
+    #if starting at XMAX YMAX
+    else:
+        for a, b in x_reverse:
+            if a != None:
+                print('G1 Y0')
+                print('G1 X{}'.format(a))
+                print('G1 Y{}'.format(args.y_max))
+                print('G1 X{}'.format(b))
+            else:
+                print('G1 Y0')
+                print('G1 X{}'.format(b))
 
 #g-code footer
 print('')
+print('; FOOTER')
 print('G28 z; home z axis')
 print('M5 $-1; all spindles off')
 print('G28 x y; home x y at the same time ')
--- sample.gcode
+++ sample.gcode
@@ -1,68 +1,40 @@
+; HEADER
+; AUTHOR: export from ggg.py
+; PART #: grid-01-01
 G54; Work Coordinates
 G21; mm mode
 G90; absolute positioning
 G28 z; home z axis
-;G28 x y; home x y at the same time - not used, assume set start positoin manually 
-G0 Z5; rapid move to specified safe z height
+;G28 x y; home x y at the same time - not used, assume set start position manually 
 M3 S3000 $-1; start all spindles clockwise
-F3000; set feedrate
-
-; X AXIS
-G1 Z0 F500; slowly drop cutter into work piece
-G1 Y0
-G1 X100
-G1 Y10
-G1 X0
-G1 Y20
-G1 X100
-G1 Y30
-G1 X0
-G1 Y40
-G1 X100
-G1 Y50
-G1 X0
-G1 Y60
-G1 X100
-G1 Y70
-G1 X0
-G1 Y80
-G1 X100
-G1 Y90
-G1 X0
-G1 Y100
-G1 X100
-G1 Y100
-G1 X0
-G0 Z5; rapid lift cutter
-G0 Y0; rapid reset y
+F500; set feedrate
+G1 Z-10.0 F200; slowly drop cutter to final cut depth
 
 ; Y AXIS
-G1 Z0 F500; slowly drop cutter into work piece
 G1 X0
 G1 Y100
-G1 X10
+G1 X-10
 G1 Y0
-G1 X20
+G1 X-20
 G1 Y100
-G1 X30
+G1 X-30
 G1 Y0
-G1 X40
+G1 X-40
 G1 Y100
-G1 X50
+G1 X-50
 G1 Y0
-G1 X60
+G1 X-60
 G1 Y100
-G1 X70
+G1 X-70
 G1 Y0
-G1 X80
+G1 X-80
 G1 Y100
-G1 X90
+G1 X-90
 G1 Y0
-G1 X100
+G1 X-100
 G1 Y100
-G1 X100
-G1 Y0
 
+; FOOTER
 G28 z; home z axis
 M5 $-1; all spindles off
 G28 x y; home x y at the same time 
