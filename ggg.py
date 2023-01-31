8074175ecccd85a8580eb9c6a9d51d0b28bab6c5
pushbutton <steve@pushbutton.com>
2023-01-31 15:24:39 -0800

    added files tested working on leadcnc 1515

--- ggg.py
+++ ggg.py
@@ -9,9 +9,11 @@ useful for
 MODULE FEATURES
 accepts X and Y maximum values to define rectangle
 accepts different pitchs to define gap between cuts in X and Y
+accepts Z value for cutting depth
 accepts one feed speed for all G1 cutting passes
 accepts only X and only Y for half grids
-assumes XY start position will be set manually
+accepts option for air traverse between grid lines or cutting them
+assumes XYZ start position will be set manually
 
 USAGE
 ./ggg.py -x -100 -y 100 --x-pitch -10 --y-pitch 10 -f 500 -z -10 --only-y --air-traverse > sample.gcode
@@ -24,7 +26,7 @@ parser.add_argument('--y-max', '-y', type=int, required=True, help='Y working wi
 parser.add_argument('--x-pitch', '-a', type=int, required=True, help='pitch between grid (stepover distance) in mm')
 parser.add_argument('--y-pitch', '-b', type=int, required=True, help='pitch between grid (stepover distance) in mm')
 parser.add_argument('--feed', '-f', type=int, required=True, help='horizontal feed in mm per minute')
-parser.add_argument('--z-depth', '-z', type=float, default=0, help='Z depth to cut (default 0 for air cut)')
+parser.add_argument('--z-depth', '-z', type=float, default=0, help='Z depth to cut (default 0)')
 parser.add_argument('--only-x', default=False, action='store_true', help='surface only in X (default both)')
 parser.add_argument('--only-y', default=False, action='store_true', help='surface only in Y (default both)')
 parser.add_argument('--air-traverse', default=False, action='store_true', help='raises cutter when traversing (default cuts traverses)')
@@ -39,24 +41,24 @@ if args.x_max < 0 and args.x_pitch >= 0 or args.x_max >=0 and args.x_pitch < 0:
     raise ValueError('Both axis max & pitch must have the same sign')
 if args.y_max < 0 and args.y_pitch >= 0 or args.y_max >=0 and args.y_pitch < 0:
     raise ValueError('Both axis max & pitch must have the same sign')
-if args.feed < 500 or args.feed > 1500:
-    print("Are you sure a feed rate of {} mm per minute is correct?".format(args.feed))
-    input('press any key to continue')
-if args.z_depth > 80:
-    print("Are you sure a Z depth of {} mm is correct?".format(args.z_depth))
-    input('press any key to continue')
+#if args.feed < 500 or args.feed > 1500:
+    #current usage redirects stdout, swap to python file manupulation to present errors
+    #print("Are you sure a feed rate of {} mm per minute is correct?".format(args.feed), flush=True)
+    #input("press any key to continue")
+#if args.z_depth < -90:
+    #print("Are you sure a Z depth of {} mm is correct?".format(args.z_depth), flush=True)
+    #input("press any key to continue")
 
 #g-code header
 print('; HEADER')
 print('; AUTHOR: export from ggg.py')
 print('; PART #: grid-01-01')
-print('G54; Work Coordinates')
+print('G54; work Coordinates')
 print('G21; mm mode')
 print('G90; absolute positioning')
-print('G28 z; home z axis')
-print(';G28 x y; home x y at the same time - not used, assume set start position manually ')
-print('M3 S3000 $-1; start all spindles clockwise')
-print('F{}; set feedrate'.format(args.feed))
+print(';G28 Z0; home z axis - not used, assume set start position manually')
+print(';G28 X0 Y0; home x y at the same time - not used, assume set start position manually ')
+print('M3 S3000; start default spindle 0 clockwise at given RPM')
 
 #return list of cordinates between 0 and end by pitch
 def steps(end, pitch):
@@ -94,21 +96,21 @@ if args.only_y == False:
     print('G1 Z{} F200; slowly drop cutter to final cut depth'.format(args.z_depth))
     for a, b in y_pairs:
         if b != None:
-            print('G1 Y{}'.format(a))
+            print('G1 Y{} F{}'.format(a, args.feed))
             if args.air_traverse == True: print('G1 Z{} F200'.format(args.z_depth))
-            print('G1 X{}'.format(args.x_max))
+            print('G1 X{} F{}'.format(args.x_max, args.feed))
             if args.air_traverse == True: print('G0 Z{}'.format(args.z_safe))
-            print('G1 Y{}'.format(b))
+            print('G1 Y{} F{}'.format(b, args.feed))
             if args.air_traverse == True: print('G1 Z{} F200'.format(args.z_depth))
-            print('G1 X0')
+            print('G1 X0 F{}'.format(args.feed))
             if args.air_traverse == True: print('G0 Z{}'.format(args.z_safe))
             #save ending positions
             y_cur = b
             x_cur = 0
         else:
-            print('G1 Y{}'.format(a))
+            print('G1 Y{} F{}'.format(a, args.feed))
             if args.air_traverse == True: print('G1 Z{} F200'.format(args.z_depth))
-            print('G1 X{}'.format(args.x_max))
+            print('G1 X{} F{}'.format(args.x_max, args.feed))
             if args.air_traverse == True: print('G0 Z{}'.format(args.z_safe))
             #save ending positions
             y_cur = a
@@ -122,58 +124,58 @@ if args.only_x == False:
     if x_cur == 0 and y_cur == 0:
         for a, b in x_pairs:
             if b != None:
-                print('G1 X{}'.format(a))
+                print('G1 X{} F{}'.format(a, args.feed))
                 if args.air_traverse == True: print('G1 Z{} F200'.format(args.z_depth))
-                print('G1 Y{}'.format(args.y_max))
+                print('G1 Y{} F{}'.format(args.y_max, args.feed))
                 if args.air_traverse == True: print('G0 Z{}'.format(args.z_safe))
-                print('G1 X{}'.format(b))
+                print('G1 X{} F{}'.format(b, args.feed))
                 if args.air_traverse == True: print('G1 Z{} F200'.format(args.z_depth))
-                print('G1 Y0')
+                print('G1 Y0 F{}'.format(args.feed))
                 if args.air_traverse == True: print('G0 Z{}'.format(args.z_safe))
             else:
-                print('G1 X{}'.format(a))
+                print('G1 X{} F{}'.format(a, args.feed))
                 if args.air_traverse == True: print('G1 Z{} F200'.format(args.z_depth))
-                print('G1 Y{}'.format(args.y_max))
+                print('G1 Y{} F{}'.format(args.y_max, args.feed))
                 if args.air_traverse == True: print('G0 Z{}'.format(args.z_safe))
     #if starting at X0 YMAX
     elif x_cur == 0:
         for a, b in x_pairs:
             if b != None:
-                print('G1 X{}'.format(a))
+                print('G1 X{} F{}'.format(a, args.feed))
                 if args.air_traverse == True: print('G1 Z{} F200'.format(args.z_depth))
-                print('G1 Y0')
+                print('G1 Y0 F{}'.format(args.feed))
                 if args.air_traverse == True: print('G0 Z{}'.format(args.z_safe))
-                print('G1 X{}'.format(b))
+                print('G1 X{} F{}'.format(b, args.feed))
                 if args.air_traverse == True: print('G1 Z{} F200'.format(args.z_depth))
-                print('G1 Y{}'.format(args.y_max))
+                print('G1 Y{} F{}'.format(args.y_max, args.feed))
                 if args.air_traverse == True: print('G0 Z{}'.format(args.z_safe))
             else:
-                print('G1 X{}'.format(a))
+                print('G1 X{} F{}'.format(a, args.feed))
                 if args.air_traverse == True: print('G1 Z{} F200'.format(args.z_depth))
-                print('G1 Y0')
+                print('G1 Y0 F{}'.format(args.feed))
                 if args.air_traverse == True: print('G0 Z{}'.format(args.z_safe))
     #if starting at XMAX YMAX
     else:
         for a, b in x_reverse:
             if a != None:
-                print('G1 Y0')
+                print('G1 Y0 F{}'.format(args.feed))
                 if args.air_traverse == True: print('G1 Z{} F200'.format(args.z_depth))
-                print('G1 X{}'.format(a))
+                print('G1 X{} F{}'.format(a, args.feed))
                 if args.air_traverse == True: print('G0 Z{}'.format(args.z_safe))
-                print('G1 Y{}'.format(args.y_max))
+                print('G1 Y{} F{}'.format(args.y_max, args.feed))
                 if args.air_traverse == True: print('G1 Z{} F200'.format(args.z_depth))
-                print('G1 X{}'.format(b))
+                print('G1 X{} F{}'.format(b, args.feed))
                 if args.air_traverse == True: print('G0 Z{}'.format(args.z_safe))
             else:
-                print('G1 Y0')
+                print('G1 Y0 F{}'.format(args.feed))
                 if args.air_traverse == True: print('G1 Z{} F200'.format(args.z_depth))
-                print('G1 X{}'.format(b))
+                print('G1 X{} F{}'.format(b, args.feed))
                 if args.air_traverse == True: print('G0 Z{}'.format(args.z_safe))
 
 #g-code footer
 print('')
 print('; FOOTER')
-print('G28 z; home z axis')
-print('M5 $-1; all spindles off')
-print('G28 x y; home x y at the same time ')
+print('G28 Z0; home z axis')
+print('M5; stop default spindle 0')
+print('G28 X0 Y0; home x y at the same time ')
 print('M2; end program')
---
+++ leadcnc_1515/grid.gcode
@@ -0,0 +1,77 @@
+; HEADER
+; AUTHOR: export from ggg.pyi by steve on 31/01/23
+; PART #: grid-01-01
+; DESCRIPTION: cuts a 100mm spaced XY grid onto leadCNC 1515 spoilboard using 45* 'V' cutter
+;              z height and xy extends are -97.06Z, 1195X, -1295Y so check these beforehand 
+G54; work Coordinates
+G21; mm mode
+G90; absolute positioning
+;G28 Z0; home z axis - not used, assume set start position manually
+;G28 X0 Y0; home x y at the same time - not used, assume set start position manually 
+M3 S3000; start default spindle 0 clockwise at given RPM
+
+; X AXIS
+G1 Z-97.06 F200; slowly drop cutter to final cut depth
+G1 Y0 F2000
+G1 X1195 F2000
+G1 Y-100 F2000
+G1 X0 F2000
+G1 Y-200 F2000
+G1 X1195 F2000
+G1 Y-300 F2000
+G1 X0 F2000
+G1 Y-400 F2000
+G1 X1195 F2000
+G1 Y-500 F2000
+G1 X0 F2000
+G1 Y-600 F2000
+G1 X1195 F2000
+G1 Y-700 F2000
+G1 X0 F2000
+G1 Y-800 F2000
+G1 X1195 F2000
+G1 Y-900 F2000
+G1 X0 F2000
+G1 Y-1000 F2000
+G1 X1195 F2000
+G1 Y-1100 F2000
+G1 X0 F2000
+G1 Y-1200 F2000
+G1 X1195 F2000
+G1 Y-1295 F2000
+G1 X0 F2000
+
+; Y AXIS
+G1 Z-97.06 F200; slowly drop cutter to final cut depth
+G1 X0 F2000
+G1 Y0 F2000
+G1 X100 F2000
+G1 Y-1295 F2000
+G1 X200 F2000
+G1 Y0 F2000
+G1 X300 F2000
+G1 Y-1295 F2000
+G1 X400 F2000
+G1 Y0 F2000
+G1 X500 F2000
+G1 Y-1295 F2000
+G1 X600 F2000
+G1 Y0 F2000
+G1 X700 F2000
+G1 Y-1295 F2000
+G1 X800 F2000
+G1 Y0 F2000
+G1 X900 F2000
+G1 Y-1295 F2000
+G1 X1000 F2000
+G1 Y0 F2000
+G1 X1100 F2000
+G1 Y-1295 F2000
+G1 X1195 F2000
+G1 Y0 F2000
+
+; FOOTER
+G28 Z0; home z axis
+M5; stop default spindle 0
+G28 X0 Y0; home x y at the same time 
+M2; end program
